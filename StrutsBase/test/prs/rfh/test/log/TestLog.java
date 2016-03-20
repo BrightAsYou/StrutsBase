445ec65357995f20ef1c8bbc63058fd51ad70445
renfuhao01 <renfuhao01@163.com>
2016-03-20 18:45:00 +0800

    添加slf4j支持

---
+++ StrutsBase/test/prs/rfh/test/log/TestLog.java
@@ -0,0 +1,12 @@
+package prs.rfh.test.log;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TestLog {
+	public static final Logger log = LoggerFactory.getLogger(TestLog.class);
+	public static void main(String[] args) {
+		log.debug("hello");
+	}
+	
+}
